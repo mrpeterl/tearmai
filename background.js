e531a3fbee31e9e8089d62ae5f4f2f3e49eaec1f
Peter Lappin <peterl@kainos.com>
2018-10-03 13:47:06 +0100

    Changed manifest added basic shape to extension

--- background.js
+++ background.js
@@ -9,12 +9,11 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log("The color is green.");
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
 });
