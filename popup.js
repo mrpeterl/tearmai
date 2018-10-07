36da95488f2d2037715530dc9987ad5be274535d
Peter Lappin <peterl@kainos.com>
2018-10-07 19:14:44 +0100

    Finally got request to return the json

--- popup.js
+++ popup.js
@@ -7,24 +7,33 @@
 let searchBtn = document.getElementById('searchBtn');
 let form = document.getElementById('searchForm');
 
-searchBtn.onclick = function(){
+searchBtn.onclick = function () {
     let language = document.getElementById('languageSelect').value;
     let word = document.getElementById('inputWord').value;
     console.log(language);
     console.log(word);
     let response = makeRequest(language, word);
-    console.log(response);
-    
+    document.getElementById('legend').innerHTML = response;
 }
 
+var getJSON = function (url) {
+    var xhr = new XMLHttpRequest();
 
-function makeRequest(language, word){
-    var params = { term:word, lang:language };
-    var str = jQuery.param( params );
+    xhr.open("GET", url, false);
+    xhr.send();
+    
+    var result = xhr.responseText;
+    return result;
+};
+
+function makeRequest(language, word) {
+    var params = { term: word, lang: language, limit: 3 };
+    var str = jQuery.param(params);
     var theUrl = "http://www.syzible.com/tearma/backend?";
     theUrl = theUrl.concat(str);
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
+
+    console.log(theUrl);
+
+    var json = getJSON(theUrl);
+    console.log(json);
+};
